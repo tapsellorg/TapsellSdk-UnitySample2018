8fd9342af535f01bee5052d0d61b4a0db20e4f39
mohsen afshar <mohsenafshar_ph@yahoo.com>
2020-07-29 22:18:26 +0430

    sample with edm4u added

---
+++ EDM/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    public void ChangeScenes (string name) {
+        SceneManager.LoadScene (name);
+    }
+}
---
+++ EDM/Assets/Scripts/TapsellInitializer.cs
@@ -0,0 +1,11 @@
+﻿using TapsellSDK;
+using UnityEngine;
+
+public class TapsellInitializer : MonoBehaviour {
+
+  private readonly string TAPSELL_KEY = "kilkhmaqckffopkpfnacjkobgrgnidkphkcbtmbcdhiokqetigljpnnrbfbnpnhmeikjbq";
+
+  void Start () {
+    Tapsell.Initialize (TAPSELL_KEY);
+  }
+}
